f47d7e4714eaa05197eef81147ee4d9c5ab3bdee
theMistletoe <katachiwotodomete@gmail.com>
2023-02-12 10:46:10 +0900

    add Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Authenticator } from '@aws-amplify/ui-react'
 import { useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
 import { NavBar, NoteCreateForm, NoteUICollection, NoteUpdateForm } from './ui-components'
@@ -9,50 +10,59 @@ function App() {
   const isDesktop: boolean = useMediaQuery({ query: '(min-width: 768px)' })
 
   return (
-    <>
-      <NavBar width="100%" marginBottom={'20px'} overrides={{
-        Create: {
-          onClick: () => {
-            setShowCreateModal(true);
-          }
-        }
-      }} />
-      <div className='container'>
-        <NoteUICollection type={isDesktop ? "grid" : "list"} overrideItems={({ item }) => {
-          return {
-            overrides: {
-              NoteUI: {
-                className: !isDesktop ? 'note-ui-mobile' : ''
-              },
-              title: {
-                color : 'blue',
-                onClick: () => {
-                  alert('title clicked');
-                }
-              },
-              edit: {
-                color: 'green',
-                onClick: () => {
-                  alert('edit clicked');
-                  setShowUpdateModal(true);
-                  setUpdateNote(item);
-                }
+    <Authenticator>
+      {({ signOut, user }) => (
+        <>
+          <NavBar width="100%" marginBottom={'20px'} overrides={{
+            Create: {
+              onClick: () => {
+                setShowCreateModal(true);
               }
             }
-          }
-        }} />  
-      </div>
-      {showCreateModal && (
-        <div className='modal'>
-          <NoteCreateForm />
-        </div>
+          }} />
+          <div className='container'>
+            <NoteUICollection type={isDesktop ? "grid" : "list"} overrideItems={({ item }) => {
+              return {
+                overrides: {
+                  NoteUI: {
+                    className: !isDesktop ? 'note-ui-mobile' : ''
+                  },
+                  title: {
+                    color : 'blue',
+                    onClick: () => {
+                      alert('title clicked');
+                    }
+                  },
+                  edit: {
+                    color: 'green',
+                    onClick: () => {
+                      alert('edit clicked');
+                      setShowUpdateModal(true);
+                      setUpdateNote(item);
+                    }
+                  }
+                }
+              }
+            }} />  
+          </div>
+          {showCreateModal && (
+            <div className='modal'>
+              <NoteCreateForm />
+            </div>
+          )}
+          {showUpdateModal && (
+            <div className='modal'>
+              <NoteUpdateForm note={updateNote} />
+            </div>
+          )}
+          <div>
+            <button onClick={() => {
+              signOut && signOut();
+            }}>SignOut</button>
+          </div>
+        </>
       )}
-      {showUpdateModal && (
-        <div className='modal'>
-          <NoteUpdateForm note={updateNote} />
-        </div>
-      )}
-    </>
+    </Authenticator>
   )
 }
 
